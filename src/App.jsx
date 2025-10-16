6031623ebb1dbceca2673041d68b0050c526b19a
Esraa Soliman <ee341110@gmail.com>
2025-10-16 12:47:35 +0300

    remove layout from login/register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useLocation } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  return (
+  const location = useLocation();
+  const noLayoutRoutes = ["/login", "/register"];
+  const isNoLayout = noLayoutRoutes.includes(location.pathname);
+
+  return isNoLayout ? (
+    <AppRouter />
+  ) : (
     <MainLayout>
       <AppRouter />
     </MainLayout>

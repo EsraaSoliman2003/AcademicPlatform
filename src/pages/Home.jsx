ae42e61524c12cc84beb2b12c9539b64abeb4331
Esraa Soliman <ee341110@gmail.com>
2025-10-17 23:11:01 +0300

    change logo icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,80 @@
+import { useEffect, useState } from "react";
 import HomeMain from "../components/Home/HomeMain";
 import CategoriesSection from "../components/Home/CategoriesSection";
 import MostViewedCourses from "../components/Home/MostViewedCourses";
 import PlatformStats from "../components/Home/PlatformStats";
 
 export default function Home() {
-  const circles = [
-    { size: 180, left: 10, top: 10, opacity: 0.35, duration: 28, delay: 2 },
-    { size: 120, left: 35, top: 5, opacity: 0.25, duration: 32, delay: 5 },
-    { size: 90, left: 70, top: 8, opacity: 0.4, duration: 25, delay: 3 },
-    { size: 160, left: 50, top: 15, opacity: 0.3, duration: 30, delay: 1 },
-    { size: 110, left: 85, top: 5, opacity: 0.45, duration: 26, delay: 4 },
+  const [isMobile, setIsMobile] = useState(false);
 
-    { size: 200, left: 5, top: 35, opacity: 0.25, duration: 35, delay: 6 },
-    { size: 130, left: 25, top: 40, opacity: 0.38, duration: 24, delay: 2 },
-    { size: 90, left: 60, top: 42, opacity: 0.32, duration: 27, delay: 1 },
-    { size: 150, left: 80, top: 30, opacity: 0.28, duration: 22, delay: 3 },
-    { size: 100, left: 45, top: 50, opacity: 0.4, duration: 29, delay: 5 },
-    { size: 70, left: 15, top: 55, opacity: 0.3, duration: 31, delay: 2 },
-    { size: 95, left: 25, top: 45, opacity: 0.37, duration: 33, delay: 3 },
-    { size: 140, left: 75, top: 48, opacity: 0.26, duration: 21, delay: 4 },
-    { size: 200, left: 90, top: 25, opacity: 0.3, duration: 36, delay: 2 },
-    { size: 200, left: 20, top: 25, opacity: 0.3, duration: 36, delay: 2 },
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    { size: 220, left: 10, top: 70, opacity: 0.28, duration: 38, delay: 3 },
-    { size: 160, left: 35, top: 75, opacity: 0.33, duration: 29, delay: 5 },
-    { size: 120, left: 55, top: 80, opacity: 0.3, duration: 32, delay: 4 },
-    { size: 100, left: 75, top: 85, opacity: 0.35, duration: 27, delay: 2 },
-    { size: 140, left: 90, top: 70, opacity: 0.25, duration: 34, delay: 6 },
-    { size: 90, left: 5, top: 90, opacity: 0.4, duration: 23, delay: 1 },
-    { size: 130, left: 25, top: 95, opacity: 0.36, duration: 31, delay: 3 },
-    { size: 180, left: 50, top: 92, opacity: 0.3, duration: 26, delay: 5 },
-    { size: 110, left: 70, top: 95, opacity: 0.27, duration: 28, delay: 4 },
-    { size: 150, left: 85, top: 88, opacity: 0.29, duration: 33, delay: 2 },
+  const desktopCircles = [
+    { size: 200, left: 5, top: 10, opacity: 0.25, duration: 36, delay: 3 },
+    { size: 150, left: 20, top: 8, opacity: 0.3, duration: 25, delay: 1 },
+    { size: 180, left: 40, top: 5, opacity: 0.35, duration: 28, delay: 4 },
+    { size: 220, left: 70, top: 12, opacity: 0.3, duration: 32, delay: 6 },
+    { size: 170, left: 85, top: 8, opacity: 0.25, duration: 31, delay: 2 },
+
+    { size: 90, left: 10, top: 25, opacity: 0.4, duration: 20, delay: 5 },
+    { size: 130, left: 35, top: 25, opacity: 0.3, duration: 26, delay: 2 },
+    { size: 100, left: 60, top: 22, opacity: 0.28, duration: 23, delay: 3 },
+    { size: 140, left: 78, top: 30, opacity: 0.35, duration: 27, delay: 1 },
+
+    { size: 200, left: 8, top: 50, opacity: 0.28, duration: 34, delay: 4 },
+    { size: 180, left: 30, top: 55, opacity: 0.3, duration: 32, delay: 5 },
+    { size: 150, left: 55, top: 50, opacity: 0.25, duration: 30, delay: 2 },
+    { size: 120, left: 75, top: 52, opacity: 0.3, duration: 29, delay: 1 },
+    { size: 100, left: 90, top: 48, opacity: 0.35, duration: 26, delay: 3 },
+
+    { size: 220, left: 5, top: 70, opacity: 0.3, duration: 35, delay: 2 },
+    { size: 180, left: 25, top: 72, opacity: 0.25, duration: 33, delay: 5 },
+    { size: 150, left: 45, top: 75, opacity: 0.28, duration: 29, delay: 1 },
+    { size: 120, left: 70, top: 68, opacity: 0.3, duration: 24, delay: 3 },
+    { size: 160, left: 85, top: 65, opacity: 0.26, duration: 27, delay: 4 },
+
+    { size: 90, left: 15, top: 85, opacity: 0.3, duration: 31, delay: 2 },
+    { size: 100, left: 35, top: 90, opacity: 0.4, duration: 29, delay: 5 },
+    { size: 130, left: 60, top: 88, opacity: 0.32, duration: 28, delay: 3 },
+    { size: 110, left: 80, top: 92, opacity: 0.25, duration: 30, delay: 6 },
+    { size: 150, left: 50, top: 95, opacity: 0.3, duration: 35, delay: 4 },
   ];
+
+  const mobileCircles = [
+    { size: 130, left: 8, top: 4, opacity: 0.32, duration: 32, delay: 2 },
+    { size: 110, left: 28, top: 6, opacity: 0.35, duration: 27, delay: 1 },
+    { size: 90, left: 55, top: 3, opacity: 0.28, duration: 25, delay: 3 },
+    { size: 120, left: 78, top: 7, opacity: 0.3, duration: 30, delay: 4 },
+
+    { size: 100, left: 12, top: 22, opacity: 0.35, duration: 26, delay: 2 },
+    { size: 80, left: 35, top: 18, opacity: 0.3, duration: 28, delay: 1 },
+    { size: 140, left: 65, top: 25, opacity: 0.25, duration: 33, delay: 5 },
+    { size: 100, left: 85, top: 28, opacity: 0.28, duration: 24, delay: 3 },
+
+    { size: 130, left: 8, top: 45, opacity: 0.3, duration: 29, delay: 2 },
+    { size: 100, left: 30, top: 48, opacity: 0.27, duration: 27, delay: 4 },
+    { size: 110, left: 55, top: 50, opacity: 0.35, duration: 25, delay: 1 },
+    { size: 80, left: 80, top: 52, opacity: 0.3, duration: 31, delay: 6 },
+
+    { size: 140, left: 10, top: 70, opacity: 0.32, duration: 33, delay: 5 },
+    { size: 100, left: 40, top: 68, opacity: 0.28, duration: 29, delay: 3 },
+    { size: 100, left: 20, top: 64, opacity: 0.28, duration: 29, delay: 3 },
+    { size: 100, left: 35, top: 63, opacity: 0.28, duration: 29, delay: 3 },
+    { size: 120, left: 70, top: 72, opacity: 0.26, duration: 30, delay: 2 },
+    { size: 90, left: 88, top: 75, opacity: 0.3, duration: 26, delay: 4 },
+
+    { size: 110, left: 15, top: 88, opacity: 0.35, duration: 34, delay: 3 },
+    { size: 90, left: 50, top: 90, opacity: 0.3, duration: 28, delay: 1 },
+    { size: 120, left: 80, top: 92, opacity: 0.27, duration: 31, delay: 2 },
+    { size: 120, left: 0, top: 97, opacity: 0.27, duration: 31, delay: 2 },
+  ];
+
+  const circles = isMobile ? mobileCircles : desktopCircles;
 
   return (
     <div className="relative overflow-hidden">
